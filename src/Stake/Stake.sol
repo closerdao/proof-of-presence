0e2fed4765a1de7068714ded76b99efba16da6c8
Artur Panach <1930175+arturictus@users.noreply.github.com>
2022-04-28 19:18:19 +0100

    WIP

--- src/Stake/Stake.sol
+++ src/Stake/Stake.sol
@@ -10,32 +10,89 @@ import "@openzeppelin/contracts/utils/math/Math.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 
-contract StakedTDF is Context {
+contract StakedTDF is Context, ReentrancyGuard {
     using SafeERC20 for IERC20;
+
+    IERC20 public immutable token;
+    mapping(address => uint256) internal _balances;
+    mapping(address => StakedUnit[]) internal _staked;
+    uint256 public lockingPeriod = 1 * 86400; // one day for tests
 
     struct StakedUnit {
         uint256 timestamp;
         uint256 amount;
     }
 
-    IERC20 public token;
-    mapping(address => uint256) internal _balances;
-    mapping(address => StakedUnit[]) internal _staked;
+    struct RedeemtionState {
+        uint256 redeemable;
+        uint256 remainingBalance;
+        StakedUnit[] unRedeemable;
+    }
 
     constructor(IERC20 _token) {
         token = _token;
     }
 
     function stake(uint256 amount) public {
-        token.safeTransfer(address(this), amount);
-        _staked[_msgSender()][block.timestamp] = amount;
+        _staked[_msgSender()].push(StakedUnit(block.timestamp, amount));
         _balances[_msgSender()] += amount;
+        token.safeTransfer(address(this), amount);
         // Emit
     }
 
-    function redeem(uint256 amount) public {
-        mapping(uint256 => uint256) storage stakedFunds = _staked[_msgSender()];
+    function redeem() public returns (uint256) {
+        RedeemtionState memory result = _calculateRedeem(_msgSender());
 
-        for (uint8 i = 0; i <= stakedFunds.length; i++) {}
+        if (result.redeemable > 0) {
+            // Change the state
+            _staked[_msgSender()] = result.unRedeemable;
+            _balances[_msgSender()] = result.remainingBalance;
+            token.safeTransfer(_msgSender(), result.redeemable);
+        }
+        // EMIT
+        return result.redeemable;
+    }
+
+    function amountRedeemable(address account) public view returns (uint256) {
+        RedeemtionState memory result = _calculateRedeem(account);
+        return result.redeemable;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    function _calculateRedeem(address account) internal view returns (RedeemtionState memory) {
+        StakedUnit[] memory stakedFunds = _staked[account];
+        RedeemtionState memory state = RedeemtionState(0, 0, new StakedUnit[](0));
+        if (stakedFunds.length == 0) {
+            return state;
+        }
+
+        for (uint8 i = 0; i < stakedFunds.length; i++) {
+            StakedUnit memory elem = stakedFunds[i];
+            if (_isRedeemable(elem)) {
+                state.redeemable += elem.amount;
+            } else {
+                state.remainingBalance += elem.amount;
+                state.unRedeemable = _addUnit(state.unRedeemable, elem);
+            }
+        }
+        return state;
+    }
+
+    function _addUnit(StakedUnit[] memory acc, StakedUnit memory unit) internal pure returns (StakedUnit[] memory) {
+        uint256 length = acc.length;
+
+        StakedUnit[] memory newAcc = new StakedUnit[](length + 1);
+        for (uint8 i = 0; i < length; i++) {
+            newAcc[i] = acc[i];
+        }
+        newAcc[length] = unit;
+        return newAcc;
+    }
+
+    function _isRedeemable(StakedUnit memory unit) internal view returns (bool) {
+        return unit.timestamp + lockingPeriod >= block.timestamp;
     }
 }
