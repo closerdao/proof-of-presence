69bcb32ea7817de7a4bae982f4f100efba0ecc3f
Artur Panach <1930175+arturictus@users.noreply.github.com>
2022-05-05 10:44:16 +0100

    fast commit at: 1651743856

---
+++ src/ProofOfPresence/ProofOfPresence.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: AGPL-1.0
+
+pragma solidity 0.8.9;
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "hardhat/console.sol";
+
+contract ProofOfPresence is Context, ReentrancyGuard {
+    using SafeERC20 for IERC20;
+    struct Booking {
+        uint256 cost;
+    }
+
+    IERC20 public immutable token;
+
+    // TODO: think in year buckets to reduce 1+n complexity
+    mapping(address => uint256[]) public dates;
+    mapping(address => mapping(uint256 => Booking)) internal _bookings;
+
+    constructor(address _token) {
+        token = IERC20(_token);
+    }
+
+    function book(uint256[] memory _dates) public {
+        for (uint256 i = 0; i < _dates.length; i++) {
+            require(_dates[i] > block.timestamp, "date should be in the future");
+            require(_bookings[_msgSender()][_dates[i]].cost == uint256(0), "Booking already exists");
+            dates[_msgSender()].push(_dates[i]);
+            _bookings[_msgSender()][_dates[i]] = Booking(1 ether);
+        }
+        // Really simplistic pricing
+        token.safeTransferFrom(_msgSender(), address(this), _dates.length * 10**18);
+    }
+
+    // TODO: optimize array iteration now is 3*n complexity: horrible performance
+    function cancel(uint256[] memory _dates) public {
+        for (uint256 i = 0; i < _dates.length; i++) {
+            require(_dates[i] > block.timestamp, "Can not cancel past booking");
+            // check booking existance
+            require(_bookings[_msgSender()][_dates[i]].cost != uint256(0), "Booking does not exists");
+            // TODO: validate booking does not exists yet
+            // dates[_msgSender()].push(_dates[i]);
+            delete _bookings[_msgSender()][_dates[i]];
+        }
+        uint256[] memory _copyDates = dates[_msgSender()];
+        delete dates[_msgSender()];
+        for (uint256 i; i < _copyDates.length; i++) {
+            bool keep = true;
+            for (uint256 o; o < _dates.length; o++) {
+                // TODO: use POP to not reiterate over the whole array
+                // uint256 localDate = _copyDates.pop();
+                if (_copyDates[i] == _dates[o]) {
+                    keep = false;
+                    break;
+                }
+            }
+            if (keep) dates[_msgSender()].push(_copyDates[i]);
+        }
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        // TODO: really simplistic balance
+        return dates[account].length * 10**18;
+    }
+}
---
+++ test/ProofOfPresence.test.ts
@@ -0,0 +1,96 @@
+import {expect} from './chai-setup';
+import {deployments, getUnnamedAccounts, ethers} from 'hardhat';
+import {TDFToken, ProofOfPresence} from '../typechain';
+import {setupUser, setupUsers} from './utils';
+import {Contract} from 'ethers';
+import {parseEther} from 'ethers/lib/utils';
+import {addDays, getUnixTime} from 'date-fns';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+async function getMock(name: string, deployer: string, args: Array<any>): Promise<Contract> {
+  await deployments.deploy(name, {from: deployer, args: args});
+  return ethers.getContract(name, deployer);
+}
+
+const setup = deployments.createFixture(async (hre) => {
+  const {deployments, getNamedAccounts, ethers} = hre;
+  await deployments.fixture();
+
+  const accounts = await getNamedAccounts();
+  const users = await getUnnamedAccounts();
+  const {deployer, TDFTokenBeneficiary} = accounts;
+
+  const token: TDFToken = await ethers.getContract('TDFToken', deployer);
+  const pOP = await getMock('ProofOfPresence', deployer, [token.address]);
+  const contracts = {
+    TDFToken: token,
+    ProofOfPresence: pOP,
+  };
+
+  const tokenBeneficiary = await setupUser(TDFTokenBeneficiary, contracts);
+
+  const conf = {
+    ...contracts,
+    users: await setupUsers(users, contracts),
+    deployer: await setupUser(deployer, contracts),
+    TDFTokenBeneficiary: tokenBeneficiary,
+    accounts,
+  };
+
+  await Promise.all(
+    [users[0], users[1]].map((e) => {
+      return conf.TDFTokenBeneficiary.TDFToken.transfer(e, parseEther('10000'));
+    })
+  );
+  return conf;
+});
+
+const buildDates = (initDate: Date, amount: number) => {
+  const acc = [];
+  for (let i = 0; i < amount; i++) {
+    acc.push(getUnixTime(addDays(initDate, i)));
+  }
+  return acc;
+};
+
+describe('ProofOfPresence', () => {
+  it('book', async () => {
+    const {users, ProofOfPresence, TDFToken} = await setup();
+
+    const testBalances = async (TK: string, tkU: string, u: string) => {
+      expect(await TDFToken.balanceOf(ProofOfPresence.address)).to.eq(parseEther(TK));
+      expect(await ProofOfPresence.balanceOf(user.address)).to.eq(parseEther(tkU));
+      expect(await TDFToken.balanceOf(user.address)).to.eq(parseEther(u));
+    };
+    const user = users[0];
+    await user.TDFToken.approve(ProofOfPresence.address, parseEther('10'));
+    const init = addDays(Date.now(), 10);
+    const dates = buildDates(init, 5);
+    await user.ProofOfPresence.book(dates);
+    await testBalances('5', '5', '9995');
+  });
+  it('book and cancel', async () => {
+    const {users, ProofOfPresence, TDFToken} = await setup();
+
+    const testBalances = async (TK: string, tkU: string, u: string) => {
+      expect(await TDFToken.balanceOf(ProofOfPresence.address)).to.eq(parseEther(TK));
+      expect(await ProofOfPresence.balanceOf(user.address)).to.eq(parseEther(tkU));
+      expect(await TDFToken.balanceOf(user.address)).to.eq(parseEther(u));
+    };
+    const user = users[0];
+    await user.TDFToken.approve(ProofOfPresence.address, parseEther('10'));
+    const init = addDays(Date.now(), 10);
+    const dates = buildDates(init, 5);
+    await user.ProofOfPresence.book(dates);
+    await testBalances('5', '5', '9995');
+
+    await user.ProofOfPresence.cancel(dates);
+    await testBalances('0', '0', '10000');
+  });
+
+  it('getters', async () => {});
+
+  it('ownable', async () => {});
+
+  it('pausable', async () => {});
+});
