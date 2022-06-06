a79be7fd064b185fb4cc7796ff6f8f916f6678f8
Artur Panach <1930175+arturictus@users.noreply.github.com>
2022-06-06 20:40:30 +0100

    diamond implementation

---
+++ src/diamond/libraries/LibAppStorage.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+import {LibDiamond} from "./LibDiamond.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../../Libraries/BookingMapLib.sol";
+
+struct Deposit {
+    uint256 timestamp;
+    uint256 amount;
+}
+
+struct AppStorage {
+    IERC20 TdfToken;
+    mapping(address => BookingMapLib.UserStore) _bookings;
+    BookingMapLib.YearsStore _years;
+    mapping(address => uint256) _balances;
+    mapping(address => Deposit[]) _deposits;
+    uint256 lockingPeriod;
+}
+
+library LibAppStorage {
+    function diamondStorage() internal pure returns (AppStorage storage ds) {
+        assembly {
+            ds.slot := 0
+        }
+    }
+}
+
+contract Modifiers {
+    AppStorage internal s;
+
+    modifier onlyOwner() {
+        LibDiamond.enforceIsContractOwner();
+        _;
+    }
+}
