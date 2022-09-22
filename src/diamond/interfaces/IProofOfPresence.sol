246357ce4040197f3a21ecfeea11437da7d674ae
Artur Panach <1930175+arturictus@users.noreply.github.com>
2022-09-22 14:12:43 +0100

    rename ProofOfPresence to Booking

--- src/diamond/interfaces/IProofOfPresence.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: AGPL-1.0
-
-pragma solidity 0.8.9;
-
-interface IProofOfPresence {
-    function book(uint16[2][] calldata dates) external;
-
-    function cancel(uint16[2][] calldata dates) external;
-}
