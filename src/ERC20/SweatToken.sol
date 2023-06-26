dae3f85560d0a3d8cb5d85022f5a2f6b9c3398a9
Zitzak <marvin88gr@gmail.com>
2023-06-26 19:56:52 +0200

    feat(Sweat): add SweatToken
    Sweat token is a non-transferable ERC20 which will be used to
    reward contributors of the project

---
+++ src/ERC20/SweatToken.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: AGPL-1.0
+pragma solidity 0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "../Interfaces/ISweatToken.sol";
+
+contract SweatToken is ISweatToken, ERC20, Ownable {
+    // 20% of total TDF supply of 18600
+    uint256 public constant MAX_SUPPLY = 3720 ether;
+
+    // solhint-disable-next-line no-empty-blocks
+    constructor() ERC20("Sweat", "SW") Ownable() {}
+
+    /**
+     * Modifier which reverts when max supply is reached or if buy amount would exeed max supply
+     */
+    modifier validateMaxSupply(uint256 amount) {
+        uint256 totalSupply = totalSupply();
+
+        if (totalSupply == MAX_SUPPLY) revert SweatToken_MaxSweatSupplyReached();
+        if ((totalSupply + amount) > MAX_SUPPLY) revert SweatToken_MintAmountExceedsMaxSupply();
+        _;
+    }
+
+    /**
+     * Mint new $Sweat tokens to address
+     */
+    function mint(address account, uint256 amount) public onlyOwner validateMaxSupply(amount) {
+        _mint(account, amount);
+    }
+
+    /**
+     * @dev See {ERC20-_beforeTokenTransfer} - transfer of token is disabled
+     *
+     */
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override {
+        super._beforeTokenTransfer(from, to, amount);
+        if (from != address(0) && to != address(0)) revert SweatToken_SweatIsNonTransferable();
+    }
+}
---
+++ test/SweatToken.test.ts
@@ -0,0 +1,73 @@
+import {expect} from './chai-setup';
+import {deployments, getUnnamedAccounts, getNamedAccounts, ethers} from 'hardhat';
+import {SweatToken} from '../typechain';
+import {setupUser, setupUsers, getMock} from './utils';
+import {parseEther} from 'ethers/lib/utils';
+
+type Context = Awaited<ReturnType<typeof setup>>;
+type Signers = Context['users'];
+
+const setup = deployments.createFixture(async () => {
+  await deployments.fixture();
+  const {deployer} = await getNamedAccounts();
+  const contracts = {
+    SweatToken: <SweatToken>await ethers.getContractOrNull('SweatToken')
+      ? <SweatToken>await ethers.getContract('SweatToken')
+      : <SweatToken>await getMock('SweatToken', deployer, []),
+  };
+  const users = await setupUsers(await getUnnamedAccounts(), contracts);
+  return {
+    ...contracts,
+    users,
+    deployer: await setupUser(deployer, contracts),
+  };
+});
+
+describe('SweatToken', function () {
+  let context: Context;
+  let users: Signers;
+
+  describe('when minting tokens', () => {
+    beforeEach(async () => {
+      context = await setup();
+      ({users} = context);
+    });
+
+    it('should be able to mint', async () => {
+      await expect(context.deployer.SweatToken.mint(users[0].address, parseEther('10'))).to.not.be.reverted;
+    });
+
+    it('should not mint when not Treasury', async () => {
+      await expect(context.users[0].SweatToken.mint(users[1].address, parseEther('10'))).to.be.revertedWith(
+        'Ownable: caller is not the owner'
+      );
+    });
+    it('should revert when mint amount exceeds max supply', async () => {
+      await expect(context.deployer.SweatToken.mint(users[0].address, parseEther('3710'))).to.not.be.reverted;
+
+      await expect(context.deployer.SweatToken.mint(users[1].address, parseEther('11'))).to.be.revertedWith(
+        'SweatToken_MintAmountExceedsMaxSupply'
+      );
+    });
+    it('should not mint when max supply reached', async () => {
+      await expect(context.deployer.SweatToken.mint(users[0].address, parseEther('3720'))).to.not.be.reverted;
+
+      await expect(context.deployer.SweatToken.mint(users[1].address, parseEther('10'))).to.be.revertedWith(
+        'SweatToken_MaxSweatSupplyReached'
+      );
+    });
+  });
+  describe('When transferring tokens', () => {
+    beforeEach(async () => {
+      context = await setup();
+      ({users} = context);
+    });
+    it('should revert', async () => {
+      await expect(context.deployer.SweatToken.mint(users[0].address, parseEther('10'))).to.not.be.reverted;
+
+      await expect(context.users[0].SweatToken.transfer(users[0].address, parseEther('5'))).to.be.revertedWith(
+        'SweatToken_SweatIsNonTransferable'
+      );
+    });
+  });
+});
