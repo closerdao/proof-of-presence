eaa9478910a2f1305efa93c97893504b07fb4cc4
Zitzak <marvin88gr@gmail.com>
2023-06-29 11:40:48 +0200

    test: adapt test to reflect TDF transfer exception for Treasury

--- test/TDFToken.test.ts
+++ test/TDFToken.test.ts
@@ -7,7 +7,7 @@ import {parseEther} from 'ethers/lib/utils';
 
 const setup = deployments.createFixture(async () => {
   await deployments.fixture();
-  const {TDFTokenBeneficiary, deployer} = await getNamedAccounts();
+  const {TDFMultisig, deployer} = await getNamedAccounts();
   const contracts = {
     TDFToken: <TDFToken>await ethers.getContract('TDFToken'),
     TDFDiamond: <TDFDiamond>await ethers.getContract('TDFDiamond'),
@@ -18,7 +18,7 @@ const setup = deployments.createFixture(async () => {
     ...contracts,
     users,
     deployer: await setupUser(deployer, contracts),
-    TDFTokenBeneficiary: await setupUser(TDFTokenBeneficiary, contracts),
+    TDFMultisig: await setupUser(TDFMultisig, contracts),
   };
 });
 
@@ -66,7 +66,7 @@ describe('TDFToken', function () {
   });
   describe('TDFDiamond transfer permitter', () => {
     it('Allowed Transfers', async () => {
-      const {deployer, users, TDFToken, TDFDiamond} = await setup();
+      const {deployer, users, TDFToken, TDFDiamond, TDFMultisig} = await setup();
       // minting
       await expect(deployer.TDFToken.mint(users[0].address, parseEther('10'))).to.emit(TDFToken, 'Transfer');
       expect(await TDFToken.balanceOf(users[0].address)).to.eq(parseEther('10'));
@@ -82,11 +82,28 @@ describe('TDFToken', function () {
         .to.emit(TDFToken, 'Transfer')
         .withArgs(users[0].address, TDFDiamond.address, parseEther('1'));
       expect(await TDFToken.balanceOf(TDFDiamond.address)).to.eq(parseEther('1'));
+
+      // Transfer from Treasury to holders
+      expect(await TDFToken.balanceOf(TDFMultisig.address)).to.eq(parseEther('0'));
+      expect(await TDFToken.balanceOf(users[5].address)).to.eq(parseEther('0'));
+      expect(await TDFToken.balanceOf(users[6].address)).to.eq(parseEther('0'));
+      await expect(deployer.TDFToken.mint(TDFMultisig.address, parseEther('10')))
+        .to.emit(TDFToken, 'Transfer')
+        .withArgs(ZERO_ADDRESS, TDFMultisig.address, parseEther('10'));
+      expect(await TDFToken.balanceOf(TDFMultisig.address)).to.eq(parseEther('10'));
+      await expect(TDFMultisig.TDFToken.transfer(users[5].address, parseEther('5')))
+        .to.emit(TDFToken, 'Transfer')
+        .withArgs(TDFMultisig.address, users[5].address, parseEther('5'));
+      await expect(TDFMultisig.TDFToken.transfer(users[6].address, parseEther('5')))
+        .to.emit(TDFToken, 'Transfer')
+        .withArgs(TDFMultisig.address, users[6].address, parseEther('5'));
+      expect(await TDFToken.balanceOf(users[5].address)).to.eq(parseEther('5'));
+      expect(await TDFToken.balanceOf(users[6].address)).to.eq(parseEther('5'));
     });
     it('Not allowed transfers', async () => {
       const {deployer, users, TDFToken} = await setup();
       // minting
-      await expect(deployer.TDFToken.mint(users[0].address, parseEther('10'))).to.emit(TDFToken, 'Transfer');
+      await expect(deployer.TDFToken.mint(users[10].address, parseEther('10'))).to.emit(TDFToken, 'Transfer');
       await expect(users[10].TDFToken.transfer(users[1].address, parseEther('1'))).to.be.revertedWith('DAO');
       await expect(users[10].TDFToken.transfer(users[2].address, parseEther('1'))).to.be.revertedWith('DAO');
       await expect(users[10].TDFToken.transfer(users[3].address, parseEther('1'))).to.be.revertedWith('DAO');
